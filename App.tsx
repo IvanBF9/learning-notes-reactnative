4e4a1de8cbad312c54e395a3e44e9647ab6b7dfb
IvanBF9 <ivanbraga@outlook.fr>
2022-06-29 10:23:44 +0200

    update

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {Home, Detail} from './src/features';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Home, Detail} from './src/pages';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 
 export default function App() {
 
-  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Detail" component={Detail} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarIcon: ({color, size, focused}) => {
+            return <Ionicons name="md-checkmark-circle" size={32} color="green" />
+          },
+        }} />
+        <Tab.Screen name="Detail" component={Detail} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
